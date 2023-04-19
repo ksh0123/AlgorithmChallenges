e565ef676b7134f29f7c8a0de72717d6a19d41aa
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2023-04-19 21:51:48 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 2.66 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,39 @@
+function solution(s){
+    let string = s;
+    let strArr = string.split('');
+    
+    function checkP(char) {
+      if (char == 'p')
+        return true;
+      else if (char == 'P')
+        return true;
+      else
+        return false;
+    }
+    
+    function checkY(char) {
+      if (char == 'y')
+        return true;
+      else if (char == 'Y')
+        return true;
+      else
+        return false;
+    }
+    
+    let filterP = strArr.filter(checkP);
+    let filterY = strArr.filter(checkY);
+    console.log(filterP);
+    console.log(filterY);
+
+    let pCount = filterP.length;
+    let yCount = filterY.length;
+    
+    if(strArr.length < 51 && (pCount == yCount)){
+        return true;
+    } else if(strArr.length < 51 && pCount == 0 && yCount == 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
