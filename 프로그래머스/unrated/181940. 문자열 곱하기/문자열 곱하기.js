75d3112c2485576fb400c87d383ae4bcd5e1b2be
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2023-05-03 10:43:03 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,7 +1,4 @@
 function solution(my_string, k) {
-    let answer = []
-    for(let i = 0; i < k; i++){
-        answer.push(my_string)
-    }
-    return answer.join('')
+    var answer = '';
+    return answer;
 }
