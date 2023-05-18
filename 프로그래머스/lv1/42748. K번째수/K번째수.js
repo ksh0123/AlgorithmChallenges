6f7460e97b995a88357743219517c0d0790c0d7e
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2023-05-18 15:44:08 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,11 +1,9 @@
 function solution(array, commands) {
     let answer = []
     for (let i = 0; i < commands.length; i++){
-        var from = commands[i][0] - 1
-        var to = commands[i][1]
-        var index = commands[i][2] - 1
-        var temp = array.slice(from, to).sort((a, b) => a - b)
-        answer.push(temp[index])
+        const [from, to, index] = commands[i]
+        let temp = array.slice(from-1, to).sort((a, b) => a - b)
+        answer.push(temp[index-1])
     }
     return answer
 }
