2e35c5192d3ba912634a62ef6ba5ed42a26242f8
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2023-05-20 12:25:45 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,6 +1,5 @@
 function solution(array) {
     let sortedArray = array.sort((a,b) => a - b)
-    console.log(sortedArray)
     let index = Math.floor(sortedArray.length / 2)
     return sortedArray[index]
 }
