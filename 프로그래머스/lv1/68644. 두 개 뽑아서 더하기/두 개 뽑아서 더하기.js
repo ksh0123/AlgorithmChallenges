5bc41ae547c0fa4f7e21b18bed81ce63b4bafa4b
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2023-05-22 23:21:02 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.43 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -2,7 +2,7 @@ function solution(numbers) {
     let answer = [];
     while (numbers.length > 0) {
         let base = numbers.shift()
-        numbers.map(el => answer.includes(base + el) ? console.log(base+el) : answer.push(base + el))
+        numbers.map(el => answer.includes(base + el) ? null : answer.push(base + el))
     }
     return answer.sort((a,b)=>a-b);
 }
