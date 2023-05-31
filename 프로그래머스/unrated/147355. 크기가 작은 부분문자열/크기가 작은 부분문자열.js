460820a3fd44c2dc1734c38ec44340bf01ba390c
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2023-05-31 11:23:34 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,11 @@
+function solution(t, p) {
+    let partialString = [];
+    for (let i = 0; i < t.length; i++){
+        let substring = t.slice(i, i+p.length)
+        !partialString.includes(Number(substring)) &&
+            Number(substring) <= Number(p) && 
+            substring.length == p.length ?
+            partialString.push(substring) : null
+    }
+    return partialString.length;
+}
