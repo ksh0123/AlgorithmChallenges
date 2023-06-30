bb4fd57094505c11127f455feb1d5843f4119017
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2023-06-30 23:21:12 +0900

    [unrated] Title: 할 일 목록, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,8 @@
+function solution(todo_list, finished) {
+    var answer = [];
+    for(let i = 0; i < finished.length; i++){
+        if(finished[i] == false) answer.push(todo_list[i]);
+        else continue;
+    }
+    return answer;
+}
