939ba1534849f919a0ce5924b675995cb9e06cf1
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2023-07-04 09:08:49 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,8 +1,7 @@
 function solution(num_list, n) {
     var answer = [];
     for (let i = 0; i < num_list.length; i+=n){
-        if(num_list[i]) answer.push(num_list[i]);
-        else break
+        answer.push(num_list[i]);
     }
     return answer;
 }
