2a5ca01466a6538a3110b83470075ebaeab75b6a
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2023-07-20 17:10:23 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.py
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.py
@@ -1,9 +1,2 @@
 def solution(num_list, n):
-    answer = []
-    answer.extend(num_list[n:len(num_list)])
-    if n > 0:
-        j = 0
-        while j < n:
-            answer.append(num_list[j])
-            j += 1
-    return answer
+    return num_list[n:] + num_list[:n]
