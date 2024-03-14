9017434162b3c524f5f0563860964cb96027b8fd
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2024-03-14 23:59:58 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,22 @@
+function solution(n) {
+    let nextNum = n + 1;
+    
+    let convertBin = (_n) => {
+        return (_n >>> 0).toString(2).replace(/0/g,'');
+    }
+    
+    let compareBin = (a,b) => {
+        return a === b ? true : false;
+    }
+    
+    
+    while(1){
+        if(compareBin(convertBin(n), convertBin(nextNum))){
+            break;
+        } else {
+            nextNum++;
+        }
+    }
+    
+    return nextNum;
+}
