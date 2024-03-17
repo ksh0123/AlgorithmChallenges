544d54d244a1f38891e123a3a074893d2a75b04a
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2024-03-17 17:33:52 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,14 @@
+function solution(cards1, cards2, goal) {
+        for(let i = 0; i < goal.length; i++){
+            if(goal[i] == cards1[0]){
+                cards1.shift()
+            } else if (goal[i] == cards2[0]){
+                cards2.shift()
+            } else {
+                if(i < goal.length){
+                    return "No";
+                }
+            }
+        }
+    return "Yes";
+}
