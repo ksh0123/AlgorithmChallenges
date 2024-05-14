8ac6646ec8a5716f6803c34d50366d861d3acc8d
Shinhye Kang <shnae.k@gmail.com>
2024-05-14 16:04:58 +0900

    Update 분수의 덧셈.js

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,13 +1,21 @@
+/*
+1. Add two fractions
+2. Simplify the fractions -> Find the greatest common divisor (GCD), divide both the numerator and the denominator by the GCD.
+*/
+
 function solution(numer1, denom1, numer2, denom2) {
+    // Add two fractions
     let numerator = numer1 * denom2 + numer2 * denom1;
     let denominator = denom1 * denom2;
     
-    let div = gcd(numerator, denominator);
-    
-    return [numerator / div, denominator / div]
+    let divisor = gcd(numerator, denominator);
+
+    // Divide the numerator and denominator by the GCD
+    return [numerator / divisor, denominator / divisor]
     
 }
-    
+
+// Find the greatest common divisor (GCD)
 function gcd(a, b){
     while (b != 0){
         let temp = b;
