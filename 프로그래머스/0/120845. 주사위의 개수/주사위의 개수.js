033847ed93422519e5a1fa40d92d95b1119a75bd
Shinhye Kang <shnae.k@gmail.com>
2024-05-16 13:54:46 +0900

    Update 주사위의 개수.js

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,3 +1,4 @@
 function solution(box, n) {
-    return parseInt(box[0] / n) * parseInt(box[1] / n) * parseInt(box[2] / n)
+    // return parseInt(box[0] / n) * parseInt(box[1] / n) * parseInt(box[2] / n)
+    return box.reduce((acc,v) => acc * Math.floor(v / n), 1);
 }
