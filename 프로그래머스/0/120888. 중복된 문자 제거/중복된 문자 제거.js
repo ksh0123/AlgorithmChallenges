26cca77e1a2418652b79acfa138156adc74d7776
Shinhye Kang <shnae.k@gmail.com>
2024-05-17 11:34:27 +0900

    Update 중복된 문자 제거.js

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,7 +1,9 @@
 function solution(my_string) {
-    var answer = '';
-    my_string.split('').map((v)=>{
-        if(!answer.includes(v)) answer += v;
-    })
-    return answer;
+    // var answer = '';
+    // my_string.split('').map((v)=>{
+    //     if(!answer.includes(v)) answer += v;
+    // })
+    // return answer;
+
+    return [...new Set(my_string)].join('');
 }
