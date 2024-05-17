409087e7d022b6e5d5ea026b36562912919e6901
Shinhye Kang <shnae.k@gmail.com>
2024-05-17 11:02:49 +0900

    Update 외계행성의 나이.js

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,10 +1,17 @@
 function solution(age) {
-    const ageMap = ['a','b','c','d','e','f','g','h','i','j'];
-    var answer = '';
-    let ageArr = age.toString().split('');
-    for(let i = 0; i < ageArr.length; i++){
-        let index = ageArr[i];
-        answer += ageMap[index];
-    }
-    return answer;
+    // const ageMap = ['a','b','c','d','e','f','g','h','i','j'];
+    // var answer = '';
+    // let ageArr = age.toString().split('');
+    // for(let i = 0; i < ageArr.length; i++){
+    //     let index = ageArr[i];
+    //     answer += ageMap[index];
+    // }
+    // return answer;
+
+    return age
+    .toString()
+    .split("")
+    .map((v) => "abcdefghij"[v])
+    .join("");
+}
 }
