a793b3a9ab8467d4e0e88b2050a78b5fe50db7b0
Shinhye Kang <shnae.k@gmail.com>
2024-05-17 11:33:56 +0900

    Update 합성수 찾기.js

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,19 +1,16 @@
 function solution(n) {
-    // var answer = 0;
-    // while (n > 3){
-    //     let count = 0;
-    //     for(let i = 1; i <= n; i++){
-    //         if(n % i == 0){
-    //             count++;
-    //         } else {
-    //             continue;
-    //         }   
-    //     }
-    //     if (count >= 3) answer++;
-    //     n--;
-    // }
-    // return answer;
-
-    return [...new Set(my_string)].join('');
-
+    var answer = 0;
+    while (n > 3){
+        let count = 0;
+        for(let i = 1; i <= n; i++){
+            if(n % i == 0){
+                count++;
+            } else {
+                continue;
+            }   
+        }
+        if (count >= 3) answer++;
+        n--;
+    }
+    return answer;
 }
