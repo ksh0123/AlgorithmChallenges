39c10a7ad51e7393506efe7c129c762bd73b74e2
Shinhye Kang <shnae.k@gmail.com>
2024-05-20 21:40:41 +0900

    Update 9로 나눈 나머지.js

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -1,4 +1,5 @@
 function solution(number) {
-    let n = number.split('').reduce((acc,x) => acc += parseInt(x),0);
-    return n % 9;
+    // let n = number.split('').reduce((acc,x) => acc += parseInt(x),0);
+    // return n % 9;
+    return [...number].reduce((a,b) => +a+(+b))%9;
 }
