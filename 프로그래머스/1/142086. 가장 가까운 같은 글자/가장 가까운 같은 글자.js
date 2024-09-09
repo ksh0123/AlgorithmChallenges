0fdf8babc92bfe9e7b66374cd18cdb92117d8d4b
shnae <shnae.k@gmail.com>
2024-09-09 19:18:59 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.89 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    var answer = [];
+    let record = {};
+    for(let i = 0; i < s.length; i++){
+        let key = s[i];
+        
+        if(record[key] == undefined){
+            record[key] = i;
+            answer.push(-1);
+        } else {
+            answer.push(i - record[key]);
+            record[key] = i;
+        }
+    }
+    return answer;
+}
